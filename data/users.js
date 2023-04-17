a6b9efb443a9acd26e8e3bf959b3bc2769a284e7
chaotization <chaozheng1735@gmail.com>
2023-04-17 18:16:11 -0400

    data updated

--- data/users.js
+++ data/users.js
@@ -2,48 +2,70 @@ import {users} from '../config/mongoCollections.js';
 import bcrypt from 'bcryptjs'
 import validation from '../validationchecker.js';
 import {ObjectId} from "mongodb";
-import e from "express";
 
+const authenticationCode = "Get privilege";
 let exportedMethods = {
     /**
      *
      * @param firstName
      * @param lastName
-     * @param username
-     * @param password
      * @param email
+     * @param userName
+     * @param password
      * @param DOB
+     * @param isAdmin
+     * @param authentication
      * @returns {Promise<{createUser: boolean, userID: string}>}
      */
 
     async createUser(
         firstName,
         lastName,
-        username,
-        password,
+        userName,
         email,
-        DOB
+        password,
+        DOB,
+        isAdmin = false,
+        authentication = null
     ) {
-        firstName = validation.checkName(firstName, 'First name');
-        lastName = validation.checkName(lastName, 'Last name');
+        firstName = validation.checkLegitName(firstName, 'First name');
+        lastName = validation.checkLegitName(lastName, 'Last name');
+        userName = validation.checkName(userName, 'User Name');
         email = validation.checkEmail(email);
         password = validation.checkPassword(password);
         DOB = validation.checkDOB(DOB);
 
         const userCollection = await users();
-        const checkExisted = await this.checkUser(email, password);
-        if (checkExisted.authenticatedUser) throw "Sign in to this account or enter an email address that isn't already in user.";
-        const hashPassword = await bcrypt.hash(password, 6);
-
-        const user = {
-            firstName: firstName,
-            lastName: lastName,
-            email: email,
-            password: hashPassword,
-            DOB: DOB,
-            post_ids: [],
-            comment_ids: []
-        };
+        const checkExist = await userCollection.findOne({email: email});
+        if (checkExist) throw "Sign in to this account or enter an email address that isn't already in user.";
+        let user;
+        if (isAdmin) {
+            if (!authentication || typeof authentication !== "string" || authentication !== authenticationCode) {
+                throw "Invalid admin verification code.";
+            }
+            user = {
+                firstName: firstName,
+                lastName: lastName,
+                userName: userName,
+                email: email,
+                password: await bcrypt.hash(password, 10),
+                DOB: DOB,
+                eventIDs: [],
+                commentIDs: [],
+                isAdmin: true
+            };
+        } else {
+            user = {
+                firstName: firstName,
+                lastName: lastName,
+                userName: userName,
+                email: email,
+                password: await bcrypt.hash(password, 10),
+                DOB: DOB,
+                postIDs: [],
+                commentIDs: []
+            };
+        }
         const insertInfo = await userCollection.insertOne(user);
         if (!insertInfo.acknowledged || !insertInfo.insertedId) throw "Could not add event.";
 
@@ -63,7 +85,7 @@ let exportedMethods = {
         password = validation.checkPassword(password);
         const userCollection = await users();
         const checkExist = await userCollection.findOne({email: email});
-        if (checkExist) throw "Sign in to this account or enter an email address that isn't already in user.";
+        if (!checkExist) throw "You may have entered the wrong email address or password.";
         const checkPassword = await bcrypt.compare(
             password,
             checkExist.password
@@ -76,12 +98,14 @@ let exportedMethods = {
     async updateUser(
         firstName,
         lastName,
+        userName,
         email,
         password,
         DOB,
     ) {
-        firstName = validation.checkName(firstName, 'First name');
-        lastName = validation.checkName(lastName, 'Last name');
+        firstName = validation.checkLegitName(firstName, 'First name');
+        lastName = validation.checkLegitName(lastName, 'Last name');
+        userName = validation.checkName(userName, 'User Name');
         email = validation.checkEmail(email);
         password = validation.checkPassword(password);
         DOB = validation.checkDOB(DOB);
@@ -93,7 +117,7 @@ let exportedMethods = {
             checkExist.password
         );
         if (checkPassword) throw "Cannot be the same password as the original";
-        const hashPassword = await bcrypt.hash(password, 6);
+        const hashPassword = await bcrypt.hash(password, 10);
         const updatedInfo = await userCollection.updateOne(
             {email: email},
             {
@@ -101,6 +125,7 @@ let exportedMethods = {
                     password: hashPassword,
                     firstName: firstName,
                     lastName: lastName,
+                    userName: userName,
                     DOB: DOB
                 },
             }
@@ -116,7 +141,15 @@ let exportedMethods = {
         const userCollection = await users();
         const user = await userCollection.findOne({email: email});
         if (!user) throw `Error: ${user} not found`; //check password as well
-        return user.post_ids;
+        return user.postIDs;
+    },
+    async getEventList(email){
+        email = validation.checkEmail(email);
+        const userCollection = await users();
+        const admin = await userCollection.findOne({email: email});
+        if (!admin) throw `Error: ${admin} not found`; //check password as well
+        if(!admin.isAdmin) throw `Error: ${email} is not an administrator`
+        return admin.eventIDs;
     },
 
     async getCommentList(email) {
@@ -124,7 +157,7 @@ let exportedMethods = {
         const userCollection = await users();
         const user = await userCollection.findOne({email: email});
         if (!user) throw `Error: ${user} not found`; //check password as well
-        return user.comments_ids;
+        return user.commentIDs;
     },
 
     async getUser(email) {
@@ -151,15 +184,16 @@ let exportedMethods = {
         const userCollection = await users();
         const user = await userCollection.findOne({_id: new ObjectId(id)});
         if (!user) throw `Error: ${user} not found`; //check password as well
-        let postIdList = user.post_ids;
+        let postIdList = user.postIDs;
         postIdList.push(postId);
         const updatedInfo = await userCollection.updateOne(
             {_id: new ObjectId(id)},
-            {$set: {post_ids: postIdList}}
+            {$set: {postIDs: postIdList}}
         );
         if (!updatedInfo.acknowledged || updatedInfo.matchedCount !== 1) throw `Could not put post with that ID ${id}`;
         return true;
     },
+
 
     async removePost(id, postId) {
         id = validation.checkId(id);
@@ -167,61 +201,97 @@ let exportedMethods = {
         const userCollection = await users();
         const user = await userCollection.findOne({_id: new ObjectId(id)});
         if (!user) throw `Error: ${user} not found`; //check password as well
-        let postIdList = user.post_ids;
+        let postIdList = user.postIDs;
         if (postIdList.includes(postId)) {
             postIdList = postId.filter(elem => elem !== postId);
             const updatedInfo = await userCollection.updateOne(
                 {_id: new ObjectId(id)},
-                {$set: {post_ids: postIdList}}
+                {$set: {postIDs: postIdList}}
             );
             if (!updatedInfo.acknowledged || updatedInfo.matchedCount !== 1) throw `Error:could not remove post with that ID${id}`;
             return true;
-        }else{
-            throw `Error: postId ${postId} not found in post_ids list for user ${id}`;
+        } else {
+            throw `Error: postId ${postId} not found in postIDs list for user ${id}`;
         }
     },
 
-    async putComment(id, commentId){
+    async putEvent(id, eventId) {
+        id = validation.checkId(id);
+        eventId = validation.checkId(eventId);
+        const userCollection = await users();
+        const user = await userCollection.findOne({_id: new ObjectId(id)});
+        if (!user) throw `Error: ${user} not found`; //check password as well
+        let eventIdList = user.eventIDs;
+        eventIdList.push(eventId);
+        const updatedInfo = await userCollection.updateOne(
+            {_id: new ObjectId(id)},
+            {$set: {eventIDs: eventIdList}}
+        );
+        if (!updatedInfo.acknowledged || updatedInfo.matchedCount !== 1) throw `Could not put event with that ID ${id}`;
+        return true;
+    },
+
+    async removeEvent(id, eventId) {
+        id = validation.checkId(id);
+        eventId = validation.checkId(eventId);
+        const userCollection = await users();
+        const user = await userCollection.findOne({_id: new ObjectId(id)});
+        if (!user) throw `Error: ${user} not found`; //check password as well
+        let eventIdList = user.eventIDs;
+        if (eventIdList.includes(eventId)) {
+            eventIdList = eventId.filter(elem => elem !== eventId);
+            const updatedInfo = await userCollection.updateOne(
+                {_id: new ObjectId(id)},
+                {$set: {eventIDs: eventIdList}}
+            );
+            if (!updatedInfo.acknowledged || updatedInfo.matchedCount !== 1) throw `Error:could not remove event with that ID${id}`;
+            return true;
+        } else {
+            throw `Error: eventId ${eventId} not found in eventIDs list for user ${id}`;
+        }
+    },
+
+    async putComment(id, commentId) {
         id = validation.checkId(id);
         commentId = validation.checkId(commentId);
         const userCollection = await users();
         const user = await userCollection.findOne({_id: new ObjectId(id)});
         if (!user) throw `Error: ${user} not found`; //check password as well
-        let commentIdList = user.comment_ids;
+        let commentIdList = user.commentIDs;
         commentIdList.push(commentId);
         const updatedInfo = await userCollection.updateOne(
             {_id: new ObjectId(id)},
-            {$set: {comment_ids: commentIdList}}
+            {$set: {commentIDs: commentIdList}}
         );
         if (!updatedInfo.acknowledged || updatedInfo.matchedCount !== 1) throw `Could not put comment with that ID ${id}`;
         return true;
     },
 
-    async removeComment(id, commentId){
+    async removeComment(id, commentId) {
         id = validation.checkId(id);
         commentId = validation.checkId(commentId);
         const userCollection = await users();
         const user = await userCollection.findOne({_id: new ObjectId(id)});
         if (!user) throw `Error: ${user} not found`; //check password as well
-        let commentIdList = user.comment_ids;
+        let commentIdList = user.commentIDs;
         if (commentIdList.includes(commentId)) {
             commentIdList = commentId.filter(elem => elem !== commentId);
             const updatedInfo = await userCollection.updateOne(
                 {_id: new ObjectId(id)},
-                {$set: {post_ids: commentIdList}}
+                {$set: {commentIDs: commentIdList}}
             );
             if (!updatedInfo.acknowledged || updatedInfo.matchedCount !== 1) throw `Error:could not remove post with that ID${id}`;
             return true;
-        }else{
-            throw `Error: postId ${commentId} not found in comment_ids list for user ${id}`;
+        } else {
+            throw `Error: postId ${commentId} not found in commentIDs list for user ${id}`;
         }
     },
 
-    async removeUserById (id) {
+    async removeUserById(id) {
         id = validation.checkId(id);
         const userCollection = await users();
         const user = await userCollection.findOne({_id: new ObjectId(id)});
-        const deletionInfo = await userCollection.deleteOne({ _id: new ObjectId(id) });
+        const deletionInfo = await userCollection.deleteOne({_id: new ObjectId(id)});
         if (deletionInfo.deletedCount === 0) {
             throw `Could not delete user with id of ${id}`;
         }
@@ -231,80 +301,3 @@ let exportedMethods = {
 
 export default exportedMethods;
 
-=======
-import {ObjectId} from 'mongodb';
-import validation from './validationchecker.js';
-
-export const addUser = async (
-    firstname,
-    lastname,
-    email,
-    hashPassword,
-    dob,
-    department
-  ) => {
-    firstname = validation.checkString(firstname, 'First name');
-    lastname = validation.checkString(lastname, 'Last name');
-    email = validation.checkString(email, 'email');
-    hashPassword = validation.checkString(hashPassword, 'Password');
-    dob = validation.checkString(dob, 'date of birth');
-    department = validation.checkString(department, 'department');
-    validation.emailValidation(email);
-    
-    let newUser = {
-        firstName: firstname,
-        lastName: lastname,
-        email: email,
-        hashPassword: hashPassword,
-        dob: dob,
-        department: department
-        
-    };
-    const userCollection = await users();
-    const newInsertInformation = await userCollection.insertOne(newUser);
-    if (!newInsertInformation.insertedId) throw 'Insert failed!';
-    return 'User added successfully';
-
-};
-
-export const updateUser = async (
-    firstname,
-    lastname,
-    email,
-    hashPassword,
-    dob,
-    department
-  ) => {
-    firstname = validation.checkString(firstname, 'First name');
-    lastname = validation.checkString(lastname, 'Last name');
-    email = validation.checkString(email, 'email');
-    hashPassword = validation.checkString(hashPassword, 'Password');
-    dob = validation.checkString(dob, 'date of birth');
-    department = validation.checkString(department, 'department');
-    validation.emailValidation(email);
-    
-    var newvalues = { $set: {
-        firstName: firstname,
-        lastName: lastname,
-        email: email,
-        hashPassword: hashPassword,
-        dob: dob,
-        department: department
-    } };
-    const userCollection = await users();
-    await userCollection.updateOne({email:email},newvalues);
-    return 'User details updated  successfully';
-
-};
-
-export const getUser = async (email,password) => {
-    email = validation.checkString(email, 'email');
-    password = validation.checkString(password, 'Password');
-    
-    const userCollection = await users();
-    const user = await userCollection.findOne({email:email});
-    if (!user) throw 'Error: User not found'; //check password as well
-    return user;
-    
-    
-};
--- validationchecker.js
+++ validationchecker.js
@@ -23,7 +23,7 @@ const exportedMethods = {
         const emailPrefixRegex = /^[a-z0-9!#$%&'*+\-/=?^_`{|}~.]+@/i;
         const emailPostfixRegex = /@stevens\.edu$/i;
         if (!emailPrefixRegex.test(email)) {
-            throw "Email address should contain only letters, numbers, and common special symbols !#$%&'*+\\-/=?^_`{|} before the @ symbol"
+            throw "Email address should contain only letters, numbers, and common special symbols !#$%&'*+\\-/=?^_`{|} before the @ character"
         }
         if (!emailPostfixRegex.test(email)) {
             throw "Error: Email address should end with stevens.edu";
@@ -37,35 +37,96 @@ const exportedMethods = {
         password = password.trim();
         if (password.length < 8 || password.length > 25) throw "Password must be at least 8 characters and less than 25 characters";
         const spaceRegex = /\s/;
-        const passwordRegex = /(?=.*\d)(?=.*[A-Z])(?=.*[a-z])(?=.*\W)/;
-        if (!spaceRegex.test(password)) throw "Password must not contain whitespace";
-        if (!passwordRegex.test(password)) throw "Password must contain at least 1 uppercase character, 1 lowercase character, 1 number, and 1 special symbol";
+        const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*\W)[a-zA-Z\d\W]{8,25}$/;
+        if (spaceRegex.test(password)) throw "Password must not contain whitespace";
+        if (!passwordRegex.test(password)) throw "Password must contain at least 1 uppercase character, 1 lowercase character, 1 number, and 1 special character";
         return password;
     },
 
     checkName(name, valName) {
-        if(!name) throw `${valName} not provided`;
-        if(typeof name !== "string" || name.trim().length === 0) throw `Please provide a valid input of ${valName}`
+        if (!name) throw `${valName} not provided`;
+        if (typeof name !== "string" || name.trim().length === 0) throw `Please provide a valid input of ${valName}`
         name = name.trim();
-        const nameRegex = /^[a-zA-Z]+$/;
-        if(!nameRegex.test(name)) throw `${valName} must be only contain character a-z and A-Z`
+        const nameRegex = /^[ a-zA-Z0-9!#$%&'*+\-/=?^_`{|}~.]+$/;
+        if (!nameRegex.test(name)) throw `${valName} must be only contain character a-z and A-Z`
         return name;
     },
 
-    checkDOB(DOB){
-        if(!DOB) throw `DOB not provided`;
-        if(typeof DOB !== "string" || DOB.trim().length === 0) throw "Please provide a valid DOB"
-        const dateRegex = /^(\d{2})-(\d{2})-(\d{4})$/;
-        if(!dateRegex.test(DOB)) throw "Invalid date format, should be 'mm-dd-yyyy";
-        const[_, month, day, year] = DOB.match(dateRegex);
+    checkLegitName(name, valName) {
+        if (!name) throw `${valName} not provided`;
+        if (typeof name !== "string" || name.trim().length === 0) throw `Please provide a valid input of ${valName}`
+        name = name.trim();
+        const nameRegex = /^[a-zA-Z]+$/;
+        if (!nameRegex.test(name)) throw `${valName} must be only contain character a-z and A-Z`;
+        if (name.length < 2)
+            throw `${valName} length must greater than 2 words`;
+        if (name.length > 20)
+            throw `${valName} length must less than 20 words`;
+        return name;
+    },
 
-        const  currentDate = new Date();
+
+    checkPhrases(phrase, valName) {
+        if (!phrase) throw `${valName} not provided`;
+        if (typeof phrase !== "string" || phrase.trim().length === 0) throw `Please provide a valid input of ${valName}`
+        phrase = phrase.trim();
+        if (phrase.length < 5)
+            throw `${valName} length must greater than 5 characters`;
+        if (phrase.length > 300)
+            throw `${valName} length must less than 300 characters`;
+        return phrase;
+    },
+
+    checkDOB(DOB) {
+        if (!DOB) throw `DOB not provided`;
+        if (typeof DOB !== "string" || DOB.trim().length === 0) throw "Please provide a valid DOB"
+        const dateRegex = /^(\d{2})-(\d{2})-(\d{4})$/;
+        if (!dateRegex.test(DOB)) throw "Invalid date format, should be 'mm-dd-yyyy";
+        const [_, month, day, year] = DOB.match(dateRegex);
+
+        const currentDate = new Date();
         const inputDate = new Date(parseInt(year), parseInt(month) - 1, parseInt(day));
 
-        if(inputDate > currentDate){
+        if (inputDate > currentDate) {
             throw "Date of birth must be in the past";
         }
         return inputDate;
+    },
+
+    checkLocation(buildingName, valName) {
+        if (!buildingName) throw `${valName} not provided`;
+        if (typeof buildingName !== "string" || buildingName.trim().length === 0) throw `Please provide a valid input of ${valName}`
+        const allowedLocation = ["edwin a. stevens hall", "carnegie laboratory", "lieb building", "burchard building",
+            "mclean hall", "babbio center", "morton-pierce-kidde complex", "rocco technology center", "nicholl environmental laboratory",
+            "davidson laboratory", "gatehouse", "griffith building and building technology tower", "walker gymnasium",
+            "schaefer athletic and recreation center", "samuel c. williams library and computer center", "jacobus student center",
+            "alexander house", "colonial house"];
+        buildingName = buildingName.trim().toLowerCase();
+        if(!allowedLocation.find(loc => buildingName.startsWith(loc))){
+            throw "Location must be on Stevens Institute of Technology main campus.";
+        }
+        return buildingName;
+    },
+
+    checkCapacity(seatCapacity){
+        if (!seatCapacity) throw "seatCapacity not provided.";
+        if (typeof seatCapacity !== "number") throw "Please provide a number."
+        if (seatCapacity < 5)
+            throw "seatCapacity must greater than 10";
+        if (seatCapacity > 300)
+            throw "seatCapacity must less than 300";
+        return seatCapacity;
+    },
+
+    getDate() {
+        const date = new Date();
+        const year = date.getFullYear();
+        const month = ('0' + (date.getMonth() + 1)).slice(-2);
+        const day = ('0' + date.getDay()).slice(-2);
+        const hour = ('0' +date.getHours()).slice(-2);
+        const minute = ('0' +date.getMinutes()).slice(-2);
+        const second = ('0' +date.getSeconds()).slice(-2);
+        return `${year}-${month}-${day} ${hour}:${minute}:${second}`
     }
 
 
