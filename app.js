e892c584e12f36726cdec46efc60b8891b369f08
Atishay Jain <ajain70@stevens.edu>
2023-04-21 12:22:25 -0400

    Baseline route is working now for events.

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 import express from 'express';
 const app = express();
 import configRoutes from './routes/index.js';
-const handlebars = require('express-handlebars');
+// import handlebars from 'express-handlebars';
 
 app.use(express.json());
 
@@ -9,16 +9,18 @@ configRoutes(app);
 
 // TODO: IMPLEMENT MIDDLEWARE HERE
 
-// setting up the handlebars engile
-app.engine('handlebars', handlebars({defaultLayout: 'main'}));
-app.set('view engine', 'handlebars');
+// TODO: Uncomment the below part later on.
 
-app.use(session({
-    name: 'AuthCookie',
-    secret: 'some secret string!',
-    resave: false,
-    saveUninitialized: true
-}));
+// setting up the handlebars engile
+// app.engine('handlebars', handlebars({defaultLayout: 'main'}));
+// app.set('view engine', 'handlebars');
+
+// // app.use(session({
+// //     name: 'AuthCookie',
+// //     secret: 'some secret string!',
+// //     resave: false,
+// //     saveUninitialized: true
+// // }));
 
 app.listen(3000, () => {
     console.log("We've now got a server!");
