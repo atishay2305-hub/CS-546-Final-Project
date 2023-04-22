6fd7c54fb11752bce2757319818b6f8e18a0374b
Atishay Jain <ajain70@stevens.edu>
2023-04-22 13:52:46 -0400

    adding changes and resolving merge conflict

--- data/comments.js
+++ data/comments.js
@@ -1,5 +1,4 @@
 import {events, posts, users, comments} from "../config/mongoCollections.js";
-import {userData, eventsData, commentData} from "./index.js";
 import {userData,commentData, postData} from "./index.js";
 import validation from '../validationchecker.js';
 import { ObjectId } from "mongodb";
