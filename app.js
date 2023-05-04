e9aba7fd733b6cda7c551f0a129a15063916761a
Atishay Jain <61509695+atishay2305-hub@users.noreply.github.com>
2023-05-04 01:34:20 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,51 +1,46 @@
 import express from "express";
 import session from 'express-session';
-
 const app = express();
 import configRoutes from './routes/index.js';
-import {fileURLToPath} from 'url';
+import { fileURLToPath } from 'url';
 import exphbs from 'express-handlebars';
-
 const __filename = fileURLToPath(import.meta.url);
 import cookieParser from 'cookie-parser';
-
 app.use(cookieParser());
 import {dirname} from 'path';
-
 const __dirname = dirname(__filename);
-const staticDir = express.static(__dirname + '/public');
+const staticDir= express.static(__dirname + '/public');
 app.use('/public', staticDir);
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-
-
+const hbs = exphbs.create({
+  defaultLayout: 'main',
+  helpers: {
+      checkCategory: function (category){
+          return category === "lost&found";
+      }
+  }
+})
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 app.use('/public', staticDir);
 import multer from "multer";
 app.use(express.urlencoded({extended: true}));
 app.use('/', staticDir);
 
-
 app.use('/public', express.static(__dirname + '/public'));
 
 
-app.use('/public', express.static(__dirname + '/public'));
-app.use(express.json());
-app.use(express.urlencoded({extended: true}));
-app.engine("handlebars", exphbs.engine({defaultLayout: "main"}));
-app.set("view engine", "handlebars");
-
-const hbs = exphbs.create({
-    defaultLayout: 'main',
-    helpers: {
-        checkCategory: function (category){
-            return category === "lost&found";
-        }
-    }
-})
 
 
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
+
+
+app.use((req, res, next) => {
+  res.setHeader("Cache-Control", "no-cache, no-store, must-revalidate");
+  res.setHeader("Pragma", "no-cache");
+  res.setHeader("Expires", "0");
+next();
+});
 
 
 app.use(session({
@@ -93,38 +88,31 @@ app.use('/register', (req, res, next) => {
 app.use('/logout', (req, res) => {
   if (!req.session.userId) {
     return res.render('login');
-
   }
   req.session.destroy();
   return res.render('logout');
 });
 
-app.use('/register', (req, res, next) => {
-  if (req.session.userId) {
-    return res.redirect('/login');
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    // null for error argument
+    cb(null, 'images')
+  },
+
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + path.extname(file.originalname))
   }
-  next();
 });
 
-// Route for logging out
-app.use('/logout', (req, res) => {
-    if (!req.session.userId) {
-        return res.render('login');
-    }
-    req.session.destroy();
-    return res.render('logout');
+const upload = multer({storage: storage})
 
+app.get("/upload", (req, res) => {
+  res.render('upload')
 });
 
-configRoutes(app);
-
-app.use((req, res, next) => {
-    res.setHeader("Cache-Control", "no-cache, no-store, must-revalidate");
-    res.setHeader("Pragma", "no-cache");
-    res.setHeader("Expires", "0");
-    next();
+app.post("/upload",upload.single('image'),(req, res) => {
+  res.send("Image Uploaded")
 });
-
 
 configRoutes(app);
 
@@ -139,4 +127,3 @@ app.listen(3000, () => {
   console.log("We've now got a server!");
   console.log('Your routes will be running on http://localhost:3000');
 });
-
