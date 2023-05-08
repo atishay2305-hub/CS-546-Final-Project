85dd8a1e5c323dba89ab1325dea9c7cf4bedfaf6
shashank98p <52718108+shashank98p@users.noreply.github.com>
2023-05-08 14:38:53 -0400

    fixed comment events

--- public/js/event/commentBtn.js
+++ public/js/event/commentBtn.js
@@ -29,3 +29,54 @@ function showCommentsPopup(eventId) {
         commentsPopup.addEventListener('click', preventPopupClose);
     }
 }
+function addComment(event, id, button) {
+    event.preventDefault();
+
+    const discussionElement = button.parentNode;
+    //console.log(id);
+    const cmtErrEle = document.getElementById(`comment-error-${id}`);
+
+    //const commentInput = discussionElement.getElementById(`comment-text-${this._id}`).value;
+    const commentInput = discussionElement.querySelector(`#comment-text-${id}`).value;
+    console.log("there ---------------------->", commentInput);
+    //
+    if (!commentInput || !commentInput.trim()) {
+        cmtErrEle.innerHTML = 'Cannot submit an empty comment';
+        cmtErrEle.style.display ='block';
+        return;
+    }
+    const commentText = commentInput.trim();
+    console.log(commentText);
+    if(commentText.length > 300){
+        cmtErrEle.innerHTML = 'Comment cannot be more than 300 characters';
+        cmtErrEle.style.display ='block';
+        return; 
+    }
+    fetch(`/events/${id}/comment`, {
+        method: "post",
+        headers: {
+            Accept: 'application/json, text/plain, */*',
+            "Content-type": 'application/json'
+        },
+        body: JSON.stringify({commentText:commentText})
+    })
+    .then(response => {
+        if (response.ok) {
+            location.reload();
+        } else {
+            //return response.json()
+            alert('Bad Input Error');
+        }
+    })
+    // .then((data) => {
+    //     if (data) {
+    //         if (!data.success) {
+    //             document.getElementById(`comment-text-${this._id}`).value = data.eventName;
+    //         }
+    //     }
+    // })
+    .catch(error => {
+        alert(error.message || "Something went wrong.");
+    });
+
+}
